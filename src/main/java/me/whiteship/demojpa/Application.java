f177b3b2bc5e6c1949898f391581ac7dbaa50510
SujinHuh <juin483@naver.com>
2020-01-18 22:29:00 +0900

    22. 스프링 데이터 Common: 커스텀 리포지토리 - 1
    쿼리 메소드(쿼리 생성과 쿼리 찾아쓰기)로 해결이 되지 않는 경우 직접 코딩으로 구현 가능.
      ● 스프링 데이터 리포지토리 ​인터페이스에 기능 추가.​
      ● 스프링 데이터 리포지토리 ​기본 기능 덮어쓰기 가능.​
      ● 구현방법
        a. 커스텀 리포지토리 인터페이스 정의
        b. 인터페이스 구현 클래스 만들기 (기본 접미어는 Impl)
        c. 엔티티 리포지토리에 커스텀 리포지토리 인터페이스 추가
    
     기능 추가하기
     기본 기능 덮어쓰기
     접미어 설정하기
    
     * 접미어 설정하기
    
      - Imple 설정 바꾸기

--- src/main/java/me/whiteship/demojpa/Application.java
+++ src/main/java/me/whiteship/demojpa/Application.java
@@ -2,8 +2,10 @@ package me.whiteship.demojpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryImplementationPostfix = "Sujin")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/me/whiteship/demojpa/post/PostCustomRepositorySujin.java
+++ src/main/java/me/whiteship/demojpa/post/PostCustomRepositorySujin.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 @Transactional
-public class PostCustomRepositoryImpl implements PostCustomRepository<Post> {
+public class PostCustomRepositorySujin implements PostCustomRepository<Post> {
 
     @Autowired
     EntityManager entityManager;
