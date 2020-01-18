ede5aafb79de1f161f95dcd2e487382b5279d50e
SujinHuh <juin483@naver.com>
2020-01-18 21:17:18 +0900

    22. 스프링 데이터 Common: 커스텀 리포지토리 - 1
    쿼리 메소드(쿼리 생성과 쿼리 찾아쓰기)로 해결이 되지 않는 경우 직접 코딩으로 구현 가능.
      ● 스프링 데이터 리포지토리 ​인터페이스에 기능 추가.​
      ● 스프링 데이터 리포지토리 ​기본 기능 덮어쓰기 가능.​
      ● 구현방법
        a. 커스텀 리포지토리 인터페이스 정의
        b. 인터페이스 구현 클래스 만들기 (기본 접미어는 Impl)
        c. 엔티티 리포지토리에 커스텀 리포지토리 인터페이스 추가
    
     기능 추가하기
     기본 기능 덮어쓰기
     접미어 설정하기
    
     * 기능 추가하기

---
+++ src/test/java/me/whiteship/demojpa/post/PostRepositoryTest.java
@@ -0,0 +1,22 @@
+package me.whiteship.demojpa.post;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void crud() {
+
+        postRepository.findMyPost();
+    }
+}
